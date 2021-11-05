47ff9ea9224ed960f7dac75ccf08018da7dd1473
Gautam Veldanda <gautamveldanda@MacBook-GT.lan>
2021-11-05 14:42:11 -0400

    Building basic method definitions

--- MMTk/src/org/mmtk/policy/RegionLocal.java
+++ MMTk/src/org/mmtk/policy/RegionLocal.java
@@ -6,8 +6,8 @@ import org.vmmagic.pragma.*;
 @Uninterruptible
 public final class RegionLocal extends RegionAllocator {
 
-    protected RegionLocal(RegionSpace space, boolean allowScanning) {
-        super(space, allowScanning);
+    protected RegionLocal(RegionSpace space, int allocationKind, boolean allowScanning) {
+        super(space, allocationKind, allowScanning);
         //TODO Auto-generated constructor stub
     }
 
