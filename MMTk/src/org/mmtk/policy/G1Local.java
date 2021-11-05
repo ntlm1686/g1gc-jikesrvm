334cb40032cbd5a93dc464d897ffde19ffffbd8d
Gautam Veldanda <gautamveldanda@MacBook-GT.lan>
2021-11-05 14:39:48 -0400

    Building basic method definitions

--- MMTk/src/org/mmtk/policy/G1Local.java
+++ MMTk/src/org/mmtk/policy/G1Local.java
@@ -6,8 +6,8 @@ import org.vmmagic.pragma.*;
 @Uninterruptible
 public final class G1Local extends RegionAllocator {
 
-    protected G1Local(RegionSpace space, boolean allowScanning) {
-        super(space, allowScanning);
+    protected G1Local(RegionSpace space, int allocationKind, boolean allowScanning) {
+        super(space, allocationKind, allowScanning);
         //TODO Auto-generated constructor stub
     }
 }
