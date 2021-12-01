068bfb25ef823ad2ff564e49f7348db0c3ccbbdd
Jiajie Li <ljjsalt@gmail.com>
2021-12-01 00:01:21 -0500

    Add new line at EOF, correct notmove in RegionEvacuateLocal.java

--- MMTk/src/org/mmtk/plan/region/RegionEvacuateLocal.java
+++ MMTk/src/org/mmtk/plan/region/RegionEvacuateLocal.java
@@ -44,6 +44,6 @@ public class RegionEvacuateLocal extends TraceLocal {
 
     @Override
     public boolean willNotMoveInCurrentCollection(ObjectReference object) {
-        return Region.regionSpace.relocationRequired(object);
+        return !Region.regionSpace.relocationRequired(object);
     }
 }
