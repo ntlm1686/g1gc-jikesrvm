7f98bf2f2beed635907f9faf840111d01a897212
Gautam Veldanda <gautamveldanda@gmail.com>
2021-12-03 01:50:47 -0500

    Comment reset

--- MMTk/src/org/mmtk/policy/RegionSpace.java
+++ MMTk/src/org/mmtk/policy/RegionSpace.java
@@ -86,9 +86,9 @@ public class RegionSpace extends Space {
         } else {
             pr = new MonotonePageResource(this, start, extent, META_DATA_PAGES_PER_REGION);
         }
-        this.initializeRegions();
-        this.resetRegionLiveBytes();
-        this.resetRequireRelocation();
+        // this.initializeRegions();
+        // this.resetRegionLiveBytes();
+        // this.resetRequireRelocation();
     }
 
     /**
